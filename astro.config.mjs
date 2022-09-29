0a55ebaf5a80c062e985929ee5d7c3547559b21f
Rishav Thakur <rishav.21m@gmail.com>
2022-09-30 04:49:45 +0530

    veercel config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 import { defineConfig } from 'astro/config';
 import preact from "@astrojs/preact";
+import vercel from "@astrojs/vercel";
 
+// https://astro.build/config
 export default defineConfig({
-  integrations: [preact()],
+  integrations: [preact(), vercel()]
 });
